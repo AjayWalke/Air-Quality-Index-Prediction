086c781ecb8e2075e2efd1af142a0c69f8d12905
AjayWalke <ajaywalke49@gmail.com>
2023-11-05 15:59:34 +0530

    change-1

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ from flask import Flask, render_template, request
 import pickle
 import numpy as np
 
-# Load the Random Forest CLassifier model
-filename = 'random_forest_regression_model.pkl'
-classifier = pickle.load(open(filename, 'rb'))
-
 app = Flask(__name__) 
 
 
@@ -26,7 +22,6 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
-        print(request.form)
         city = request.form['city']
         t = int(request.form['T'])
         tm = int(request.form['TM'])
@@ -36,11 +31,52 @@ def predict():
         vv = float(request.form['VV'])
         v = float(request.form['V'])
         vm = int(request.form['VM'])
+        ans = []
+        if(city == 'pune'):
+            print(1)
+            filename = './pkl_files/Pune.pkl'
+            classifier = pickle.load(open(filename, 'rb'))
+            data = np.array([[t, tm, tmm, slp, h, vv, v, vm]])
+            my_prediction = classifier.predict(data)
+            ans.insert(my_prediction)
+        elif(city == 'nagpur'):
+            print(2)
+            filename = './pkl_files/Nagpur.pkl'
+            classifier = pickle.load(open(filename, 'rb'))
+            data = np.array([[t, tm, tmm, slp, h, vv, v, vm]])
+            my_prediction = classifier.predict(data)
+            ans.insert(my_prediction)
+        elif(city == 'delhi'):
+            print(3)
+            filename = './pkl_files/Delhi.pkl'
+            classifier = pickle.load(open(filename, 'rb'))
+            data = np.array([[t, tm, tmm, slp, h, vv, v, vm]])
+            my_prediction = classifier.predict(data)
+            ans.insert(my_prediction)
+        elif(city == 'mumbai'):
+            print(4)
+            filename = './pkl_files/Mumbai.pkl'
+            classifier = pickle.load(open(filename, 'rb'))
+            data = np.array([[t, tm, tmm, slp, h, vv, v, vm]])
+            my_prediction = classifier.predict(data)
+            ans.insert(my_prediction)
+        elif(city == 'chennai'):
+            print(4)
+            filename = './pkl_files/Chennai.pkl'
+            classifier = pickle.load(open(filename, 'rb'))
+            data = np.array([[t, tm, tmm, slp, h, vv, v, vm]])
+            my_prediction = classifier.predict(data)
+            ans.insert(my_prediction)
+        elif(city == 'bangalore'):
+            print(5)
+            filename = './pkl_files/Bangalore.pkl'
+            classifier = pickle.load(open(filename, 'rb'))
+            data = np.array([[t, tm, tmm, slp, h, vv, v, vm]])
+            my_prediction = classifier.predict(data)
+            ans.insert(my_prediction)
 
-        data = np.array([[t, tm, tmm, slp, h, vv, v, vm]])
-        my_prediction = classifier.predict(data)
-
-        return render_template('result.html', prediction=my_prediction)
+        print(ans);
+        return render_template('result.html', prediction=ans)
 
 
 if __name__ == '__main__':
