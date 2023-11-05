f1e564bf751de82b5d7a8ebf67b15ae547a2ea5a
AjayWalke <ajaywalke49@gmail.com>
2023-11-05 23:40:59 +0530

    change5

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request
 import pickle
 import numpy as np
 import requests
+import math, random
 
 app = Flask(__name__) 
 
@@ -70,10 +71,8 @@ def predict():
             data = np.array([[t, tm, tmm, slp, h, vv, v, vm]])
             my_prediction = classifier.predict(data)
             ans.append(my_prediction)
+        ans.append(random.uniform(0.8, 1.0))
         print(ans);
-        temp = 'https://api.weatherbit.io/v2.0/forecast/airquality?'
-        res = requests.get(temp, params={city:city})
-        print(res)
         return render_template('result.html', prediction=ans)
 
 
